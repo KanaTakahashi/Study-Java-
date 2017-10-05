04f3c4bf5769d81294abbf8f13ce3f7b78dfa7ec
KanaTakahashi <16ec066@ms.dendai.ac.jp>
2017-10-05 10:52:45 +0900

    Hello World
    初めてのコミット
    My First Commit

---
+++ Display.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kana
+ */
+import java.util.Scanner; //Scannerをインポート　
+public class Display {
+    
+ public static void main(String[] args) {
+ Scanner stdIn = new Scanner(System.in);
+         System.out.println("Hello World");
+         System.out.println("Githubを使用する環境が整いました");
+}
+}
+
